dd1da9d53f62ac4401dd5628acf6d5312e942d70
Junjie Liu <junjiel1@andrew.cmu.edu>
2018-11-30 22:38:52 -0800

    Working Version : Extracting Unit information from download CVS files

---
+++ target/classes/com/red/domain/PropertyStyle.java
@@ -0,0 +1,14 @@
+package com.red.domain;
+
+/**
+ * Created by Liujunjie on 11/2/18.
+ */
+public enum PropertyStyle {
+    Single,
+    Town,
+    Condo;
+
+    public String toString() {
+        return name();
+    }
+}
---
+++ target/classes/com/red/domain/SchoolLevel.java
@@ -0,0 +1,14 @@
+package com.red.domain;
+
+/**
+ * Created by Liujunjie on 2018/11/4.
+ */
+public enum SchoolLevel {
+    ELEMENTARY,
+    MIDDLE,
+    HIGH;
+
+    public String toString() {
+        return name();
+    }
+}
